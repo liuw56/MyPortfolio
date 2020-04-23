742e02b8ae9036695e2118b35821a44b804655ac
Wei Liu <liuw56@mcmaster.ca>
2020-04-22 20:23:20 -0400

    added menu overlay

--- dict/js/main.js
+++ dict/js/main.js
@@ -13,15 +13,15 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuNav.classList.add("close");
-    menuBranding.classList.add("close");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
     showMenu = true;
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuNav.classList.remove("close");
-    menuBranding.classList.remove("close");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     navItems.forEach((item) => item.classList.remove("show"));
     showMenu = false;
   }
